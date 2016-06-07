9b65b83143984360bca839b23f96ecb336cf480c
Arun Ganesh <arun.planemad@gmail.com>
2016-06-07 17:40:23 +0530

    Move common functions to external js

--- index.js
+++ index.js
@@ -42,162 +42,18 @@ var overlayData = {
 
 // Map ready
 map.on('style.load', function(e) {
-    init();
 
 
-    function init() {
+    map.addSource('overlayDataSource', overlayDataSource);
+    map.addLayer(overlayData);
 
 
-        map.addSource('overlayDataSource', overlayDataSource);
-        map.addLayer(overlayData);
-        getOverlayFeatures();
+    // inspectMapFeature(['water'], )
 
-        map.on('click', function(e) {
-
-          openLocationInJOSM(e);
-
-        });
-
-        // Add review marker on the map
-        function clickToReview(){
-
-          var newOverlayFeature = {
-              "type": "Feature",
-              "properties": {},
-              "geometry": {
-                  "coordinates": [
-
-                  ],
-                  "type": "Point"
-              }
-          };
-
-          var clickedOverlayFeatures = map.queryRenderedFeatures([
-              [e.point.x - 5, e.point.y - 5],
-              [e.point.x + 5, e.point.y + 5]
-          ], {
-              layers: ['overlayData']
-          });
-          if (clickedOverlayFeatures.length) {
-              overlayFeatureForm(clickedOverlayFeatures[0]);
-
-          } else {
-              overlayFeatureForm();
-          }
-
-          function overlayFeatureForm(feature) {
-              var formOptions = "<div class='radio-pill pill pad1y clearfix'><input id='valid' type='radio' name='review' value='tree' checked='checked'><label for='tree' class='short button icon check fill-green'>tree</label><input id='sapling' type='radio' name='review' value='sapling'><label for='sapling' class='short button icon check fill-red'>sapling</label></div>";
-              var formReviewer = "<fieldset><label>Contributed by: <span id='reviewer' style='padding:5px;background-color:#eee'></span></label><input type='text' name='reviewer' placeholder='name'></input></fieldset>"
-              var popupHTML = "<form>" + formOptions + formReviewer + "<a id='updateOverlayFeature' class='button col4' href='#'>Save</a><a id='deleteOverlayFeature' class='button quiet fr col4' href='#' style=''>Delete</a></form>";
-              var popup = new mapboxgl.Popup()
-                  .setLngLat(e.lngLat)
-                  .setHTML(popupHTML)
-                  .addTo(map);
-
-              // Show existing status if available
-              if (feature) {
-                  $("input[name=review][value=" + feature.properties["natural"] + "]").prop('checked', true);
-                  $("#reviewer").html(feature.properties["contributed_by"]);
-                  newOverlayFeature = feature;
-                  newOverlayFeature["id"] = feature.properties["id"];
-                  console.log(feature);
-              } else {
-                  newOverlayFeature.geometry.coordinates = e.lngLat.toArray();
-              }
-
-              // Set reviewer name if previously saved
-              if (reviewer) {
-                  $("input[name=reviewer]").val(reviewer);
-              }
-
-              // Update dataset with feature status on clicking save
-              document.getElementById("updateOverlayFeature").onclick = function() {
-                  newOverlayFeature.properties["natural"] = $("input[name=review]:checked").val();
-                  reviewer = $("input[name=reviewer]").val();
-                  newOverlayFeature.properties["contributed_by"] = reviewer;
-                  popup.remove();
-                  mapbox.insertFeature(newOverlayFeature, dataset, function(err, response) {
-                      console.log(response);
-                      overlayFeatureCollection.features = overlayFeatureCollection.features.concat(response);
-                      overlayDataSource.setData(overlayFeatureCollection);
-                  });
-              };
-              // Delete feature on clicking delete
-              document.getElementById("deleteOverlayFeature").onclick = function() {
-                  popup.remove();
-                  mapbox.deleteFeature(newOverlayFeature["id"], dataset, function(err, response) {
-                      console.log(response);
-                  });
-              };
-          }
-        }
-
-    }
+    map.on('click', function(e) {
 
 
-    // Get data from a Mapbox dataset
-    var overlayFeatureCollection = {
-        'type': 'FeatureCollection',
-        'features': []
-    };
 
-    function getOverlayFeatures(startID) {
-
-        var url = DATASETS_BASE + 'features';
-        var params = {
-            'access_token': mapboxAccessDatasetToken
-        };
-
-        // Begin with the last feature of previous request
-        if (startID) {
-            params.start = startID;
-        }
-
-        $.getJSON(url, params, function(data) {
-
-            console.log(data);
-
-            if (data.features.length) {
-                data.features.forEach(function(feature) {
-                    // Add dataset feature id as a property
-                    feature.properties.id = feature.id;
-                });
-                overlayFeatureCollection.features = overlayFeatureCollection.features.concat(data.features);
-                var lastFeatureID = data.features[data.features.length - 1].id;
-                getOverlayFeatures(lastFeatureID);
-                overlayDataSource.setData(overlayFeatureCollection);
-            }
-            overlayDataSource.setData(overlayFeatureCollection);
-        });
-    }
+    });
 
 });
-
-// Toggle visibility of a layer
-function toggle(id) {
-    var currentState = map.getLayoutProperty(id, 'visibility');
-    var nextState = currentState === 'none' ? 'visible' : 'none';
-    map.setLayoutProperty(id, 'visibility', nextState);
-}
-
-// Toggle a set of filters for a set of layers
-function toggleLayerFilters(layerItems, filterItem) {
-
-    for (var i in layerItems) {
-        for (var j in toggleLayers[layerItems[i]].layers) {
-
-            var existingFilter = map.getFilter(toggleLayers[layerItems[i]].layers[j]);
-
-            // Construct and add the filters if none exist for the layers
-            if (typeof existingFilter == 'undefined') {
-                map.setFilter(toggleLayers[layerItems[i]].layers[j], toggleFilters[filterItem].filter);
-            } else {
-                // Not implemented
-                var newFilter = mergeLayerFilters(existingFilter, toggleFilters[filterItem].filter);
-                map.setFilter(toggleLayers[layerItems[i]].layers[j], newFilter);
-                // console.log(newFilter);
-            }
-
-        }
-    }
-}
---
+++ mapbox-gl-utils.js
@@ -0,0 +1,103 @@
+// Utility functions to work with Mapbox GL JS maps
+// Requires jquery
+// - Toggle visibility of a layer
+
+
+// Toggle visibility of a layer
+function toggle(id) {
+    var currentState = map.getLayoutProperty(id, 'visibility');
+    var nextState = currentState === 'none' ? 'visible' : 'none';
+    map.setLayoutProperty(id, 'visibility', nextState);
+}
+
+// Hide all except one layer from a group
+function showOnlyLayers(toggleLayers, showLayerItem) {
+    for (var layerItem in toggleLayers) {
+        for (var layer in toggleLayers[layerItem].layers) {
+            if (showLayerItem == layerItem)
+                map.setLayoutProperty(toggleLayers[layerItem].layers[layer], 'visibility', 'visible');
+            else
+                map.setLayoutProperty(toggleLayers[layerItem].layers[layer], 'visibility', 'none');
+        }
+    }
+    // Highlight menu items
+    $('.toggles a').removeClass('active');
+    $('#' + showLayerItem).addClass('active');
+}
+
+// Toggle a set of filters for a set of layers
+function toggleLayerFilters(layerItems, filterItem) {
+
+    for (var i in layerItems) {
+        for (var j in toggleLayers[layerItems[i]].layers) {
+
+            var existingFilter = map.getFilter(toggleLayers[layerItems[i]].layers[j]);
+
+            // Construct and add the filters if none exist for the layers
+            if (typeof existingFilter == 'undefined') {
+                map.setFilter(toggleLayers[layerItems[i]].layers[j], toggleFilters[filterItem].filter);
+            } else {
+                // Not implemented
+                var newFilter = mergeLayerFilters(existingFilter, toggleFilters[filterItem].filter);
+                map.setFilter(toggleLayers[layerItems[i]].layers[j], newFilter);
+                // console.log(newFilter);
+            }
+
+        }
+    }
+}
+
+// Parse the toggleFilters to build the compound filter arrays
+function parseToggleFilters() {
+    for (var filterItem in toggleFilters) {
+
+        var parsedFilter = new Array();
+        parsedFilter.push(toggleFilters[filterItem]['filter-mode']);
+
+        for (var value in toggleFilters[filterItem]['filter-values']) {
+            var filter = new Array();
+            filter.push(toggleFilters[filterItem]['filter-compare'][0], toggleFilters[filterItem]['filter-compare'][1], toggleFilters[filterItem]['filter-values'][value]);
+            parsedFilter.push(filter);
+        }
+
+        toggleFilters[filterItem]['filter'] = parsedFilter;
+    }
+}
+
+// Merge two GL layer filters into one
+function mergeLayerFilters(existingFilter, mergeFilter) {
+    var newFilter = new Array();
+
+    // If the layer has a simple single filter
+    if (existingFilter[0] == '==') {
+        newFilter.push("all", existingFilter, mergeFilter)
+    }
+
+    return newFilter;
+}
+
+// Return a square bbox of pixel coordinates from a given x,y point
+function pixelPointToSquare(point, width) {
+    var pointToSquare = [
+        [point.x - width/2, point.y - width/2],
+        [point.x + width/2, point.y + width/25]
+    ];
+    return pointToSquare;
+}
+
+
+//
+// OpenStreetMap Utilities
+//
+
+//Open map location in JOSM
+function openInJOSM(opts) {
+    var bounds = map.getBounds();
+    var top = bounds.getNorth();
+    var bottom = bounds.getSouth();
+    var left = bounds.getWest();
+    var right = bounds.getEast();
+    // var josmUrl = 'https://127.0.0.1:8112/load_and_zoom?left='+left+'&right='+right+'&top='+top+'&bottom='+bottom;
+    var josmUrl = 'http://127.0.0.1:8111/load_and_zoom?left=' + left + '&right=' + right + '&top=' + top + '&bottom=' + bottom;
+    $.ajax(josmUrl, function() {});
+}
